c1ef31996a3d6a9f6c78ec00968f2e4a516cfce1
mickymck <mickymckeon@gmail.com>
2019-09-27 13:44:49 -0400

    got my hero and my flag

--- script.js
+++ script.js
@@ -3,28 +3,46 @@ class Game {
         const canvas = document.getElementById(canvasId)
         this.screen = canvas.getContext("2d")
         this.size = { width: canvas.width, height: canvas.height }
+        this.keyboard = new Keyboarder
 
         this.bodies = []
 
-        let playerSize = {
-            width: 20,
-            height: 20
+        let heroSize = {
+            width: 10,
+            height: 10
         }
 
-        let playerLocation = {
-            x: Math.floor(this.size.width * .5),
-            y: Math.floor(this.size.height - (playerSize.height * 2))
+        let heroLocation = {
+            x: Math.floor(this.size.width * .5) - (heroSize.width / 2),
+            y: Math.floor(this.size.height - (heroSize.height * 2))
         }
 
-        console.log(playerLocation)
+        let flagSize = {
+            width: 10,
+            height: 10
+        }
 
-        this.player = new Player(playerLocation, playerSize)
-        this.addBody(this.player)
+        let flagLocation = {
+            x: Math.floor(this.size.width * .5) - (flagSize.width / 2),
+            y: Math.floor(flagSize.height * 2)
+        }
+
+        console.log(flagLocation)
+
+        this.hero = new Hero(heroLocation, heroSize)
+        this.addBody(this.hero)
+
+        this.flag = new Flag(flagLocation, flagSize)
+        this.addBody(this.flag)
     }
 
     addBody(body) {
         this.bodies.push(body)
     }
+
+    // add tick, to give our players the ability to move
+
+    // add keyboardability for our Hero
 
     run() {
         this.draw()
@@ -40,7 +58,7 @@ class Game {
     }
 }
 
-class Player {
+class Hero {
     constructor(location, size) {
         this.location = location
         this.size = size
@@ -51,6 +69,46 @@ class Player {
         screen.fillRect(this.location.x, this.location.y, this.size.width, this.size.height)
     }
 }
+
+class Flag {
+    constructor(location, size) {
+        this.location = location
+        this.size = size
+    }
+
+    draw(screen) {
+        screen.fillStyle = "#8CD1EF"
+        screen.fillRect(this.location.x, this.location.y, this.size.width, this.size.height)
+    }
+}
+
+class Keyboarder {
+    constructor() {
+        this.keyState = {}
+
+        window.addEventListener('keydown', function (e) {
+            this.keyState[e.keyCode] = true
+        }.bind(this))
+
+        window.addEventListener('keyup', function (e) {
+            this.keyState[e.keyCode] = false
+        }.bind(this))
+    }
+
+    isDown(keyCode) {
+        return this.keyState[keyCode] === true
+    }
+
+    on(keyCode, callback) {
+        window.addEventListener('keydown', function (e) {
+            if (e.keyCode === keyCode) {
+                callback()
+            }
+        })
+    }
+}
+
+Keyboarder.KEYS = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, S: 83 }
 
 const captureGame = new Game("capture-the-flag")
 captureGame.run()
